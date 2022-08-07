21fe89e7edc8a4af643553d6d463555065f25a63
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-07 18:00:32 +0000

    Add update function to sprite class

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ class Sprite {
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw() {
         c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        }
     }
 }
 
@@ -25,11 +35,10 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
-player.draw()
 
 const enemy = new Sprite({
     position: {
@@ -48,7 +57,10 @@ console.log(player)
 
 function animate() {
     window.requestAnimationFrame(animate)
-    console.log("go")
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
 }
 
 animate()
